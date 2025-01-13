178207986b61bbc62ec638bcbfcfa40a3c2f1d6a
Blessed Bagor <blessedbagor.eg@gmail.com>
2025-01-14 00:33:19 +0800

    modified:   lib/utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,20 +11,7 @@ export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
 
-// //Format number with decimal places
-// export function formatNumberWithDecimal(num: number): string {
-//   num.toString().split('.');
-//   return decimal ? `${int}.${decimal}.padEnd(2, '0')` : `${int}.00`;
-// }
-
 export function formatNumberWithDecimal(num: number): string {
   const [int, decimal = ''] = num.toString().split('.');
-
-  // Format the integer part with thousands separators
-  const formattedInt = parseInt(int, 10).toLocaleString();
-
-  // Ensure the decimal part has exactly two digits
-  const formattedDecimal = decimal.padEnd(2, '0').slice(0, 2);
-
-  return `${formattedInt}.${formattedDecimal}`;
+  return `${int}.${decimal.padEnd(2, '0').slice(0, 2)}`;
 }
