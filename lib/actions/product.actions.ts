2c4d0e84f63a22d906fac75895dce74d58471ad6
Blessed Bagor <blessedbagor.eg@gmail.com>
2025-01-15 15:17:22 +0800

    Added Callback url redirect

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,16 +3,6 @@ import {prisma} from '@/db/prisma';
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
-// // Get latest products
-// export async function getLatestProducts() {
-//     const data = await prisma.product.findMany({
-//         take: LATEST_PRODUCTS_LIMIT,
-//         orderBy: {createdAt: 'desc'}
-//     });
-
-//     return convertToPlainObject(data);
-// }
-
 // Get latest products
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
