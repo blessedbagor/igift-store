90ea0bf2450d9113906bbaf6e4099390130b1f97
Blessed Bagor <blessedbagor.eg@gmail.com>
2025-02-08 17:15:27 +0800

    Fixing localhost redirection in deployment

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'iGift';
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Helping Busy Professionals Stay Healthy';
-export const SERVER_URL = process.env.NEXT_PUBLIC_APP_URL || 'https://localhost:3000';
+export const SERVER_URL = process.env.NEXT_PUBLIC_APP_URL;
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
