5f6698a85fa0c3f80526a9c387b14e6d99ff7e97
Blessed Bagor <blessedbagor.eg@gmail.com>
2025-03-23 22:59:44 +0800

    Fixed dynamic code evaluation issue.

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
+export const config = {
+    unstable_allowDynamic: [
+      // allows a single file
+      '/lib/utils.ts',
+      // use a glob to allow anything in the function-bind 3rd party module
+      '**/node_modules/function-bind/**',
+      // allow dynamic code evaluation for prisma client
+      '**/node_modules/@prisma/client/runtime/**',
+      '**/node_modules/@auth/prisma-adapter/**',
+    ],
+  }
 export {auth as middleware} from '@/auth';
